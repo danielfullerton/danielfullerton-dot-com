417ab78ab1a842785bafd94047413b4e62ab4ea7
Dan Fullerton <danfullerton@Dans-MacBook-Pro.local>
2025-04-06 14:10:16 -0400

    google analytics

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+
+const GA_MEASUREMENT_ID = "G-Q03Q6J0D5E";
+
+declare global {
+  interface Window {
+    gtag: (
+      command: string,
+      action: string,
+      params?: Record<string, any>
+    ) => void;
+  }
+}
+
+export default function GoogleAnalytics() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag("config", GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div
       className={`${inter.variable} ${inter.className} min-h-screen flex flex-col`}
     >
+      <GoogleAnalytics />
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
